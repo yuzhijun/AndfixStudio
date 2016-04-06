9485b117ace8c4dbf9f1d7639213269c6619d2fd
wenhao <wenhao@mafengwo.com>
2016-04-06 18:46:14 +0800

    添加demo并升级gradle-experimental

--- app/src/main/java/com/leochin/andfixstudio/MainActivity.java
+++ app/src/main/java/com/leochin/andfixstudio/MainActivity.java
@@ -1,52 +1,33 @@
 package com.leochin.andfixstudio;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "euler";
+
+    private TextView mTestView;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        mTestView = (TextView) findViewById(R.id.tv_test);
+
+        String vmVersion = System.getProperty("java.vm.version");
+        mTestView.setText(vmVersion);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
+    protected void onDestroy() {
+        super.onDestroy();
+        android.os.Process.killProcess(android.os.Process.myPid());
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    public void showToast(View v) {
+        A.showToast(this);
     }
 }
---
+++ tools/README.md
@@ -0,0 +1,4 @@
+使用说明
+===
+
+在tools下新建apk目录，将线上包命名为old.apk,将修复bug后的包命名为new.apk并放在apk目录下
